4979762261c29687ffee438ce1d1e39dddfff874
Kaizen06 <umlyrithyreach@gmail.com>
2025-02-21 22:54:30 +0700

    Exercise3 & Exercise4

---
+++ Exercise3/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header.jsx";
+import Main from "./components/Main.jsx";
+import Time from "./components/Time.jsx";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Main>
+        <Time></Time>
+      </Main>
+    </>
+  );
+}
+
+export default App;
